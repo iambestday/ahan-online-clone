ad2ac783c8b6ee03cce073bce0c3dbdb3b3b2bb9
iambestday <behrouz.asghari@gmail.com>
2022-08-28 23:42:20 +0430

    ✨ Add some routes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import DashboardLayout from "./layouts/dashboard/DashboardLayout";
 import HomeLayout from "./layouts/home/HomeLayout";
 import Error404 from "./layouts/pages/Error404";
 import Wiki from "./layouts/pages/Wiki"
-
+import Join from "./layouts/pages/Join"
+import Contact from "./layouts/pages/Contact";
+import About from "./layouts/pages/About";
 
 
 function App() {
@@ -15,8 +17,9 @@ function App() {
         <Route path="*" element={<Error404 />}/>
         <Route path="/dashboard" element={<DashboardLayout />}/>
         <Route path="/wiki" element={<Wiki />}/>
-        
-        
+        <Route path="/join-us" element={<Join/>}/>
+        <Route path="/about-us" element={<About/>}/>
+        <Route path="/contact-us" element={<Contact/>}/>
       </Routes>
     </BrowserRouter>
   );
