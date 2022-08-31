7e7c5a8f8db6beebe10167b59910d8d808690dd6
iambestday <behrouz.asghari@gmail.com>
2022-08-31 20:50:02 +0430

    🎨 add some nested routes

--- src/App.js
+++ src/App.js
@@ -7,19 +7,41 @@ import Wiki from "./layouts/pages/Wiki"
 import Join from "./layouts/pages/Join"
 import Contact from "./layouts/pages/Contact";
 import About from "./layouts/pages/About";
+import Signup from "./layouts/pages/Signup";
+import AddNewLayout from "./layouts/dashboard/AddNewLayout";
+import InvoicesLayout from "./layouts/dashboard/InvoicesLayout";
+import PricesLayout from "./layouts/dashboard/PricesLayout";
+import TurnoverLayout from "./layouts/dashboard/TurnoverLayout";
+import LocationsLayout from "./layouts/dashboard/LocationsLayout";
+import SupportsLayout from "./layouts/dashboard/SupportsLayout";
+import SettingsLayout from "./layouts/dashboard/SettingsLayout";
+import StatLayout from "./layouts/dashboard/StatLayout";
+import MainLayout from "./layouts/home/MainLayout";
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomeLayout />}/>
+        <Route path="/" element={<HomeLayout />}>
+        <Route index element={<MainLayout />} />
         <Route path="*" element={<Error404 />}/>
-        <Route path="/dashboard" element={<DashboardLayout />}/>
         <Route path="/wiki" element={<Wiki />}/>
         <Route path="/join-us" element={<Join/>}/>
         <Route path="/about-us" element={<About/>}/>
         <Route path="/contact-us" element={<Contact/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+        </Route>
+        <Route path="/dashboard" element={<DashboardLayout />}>
+        <Route index element={<StatLayout />} />
+        <Route path="new" element={<AddNewLayout />} />
+        <Route path="invoices" element={<InvoicesLayout />} />
+        <Route path="prices" element={<PricesLayout />} />
+        <Route path="turnover" element={<TurnoverLayout />} />
+        <Route path="locations" element={<LocationsLayout />} />
+        <Route path="supports" element={<SupportsLayout />} />
+        <Route path="settings" element={<SettingsLayout />} />
+      </Route>
       </Routes>
     </BrowserRouter>
   );
