45afb97dce34d0eab01ded8405d1fc845e685570
iambestday <behrouz.asghari@gmail.com>
2022-09-03 20:08:04 +0430

    🐛 Fix Bug

--- src/layouts/dashboard/AddNewLayout.js
+++ src/layouts/dashboard/AddNewLayout.js
@@ -1,7 +1,7 @@
-import PriceList from "../../components/dashboard/PriceList";
-import ProductList from "../../components/dashboard/ProductList";
-import Steps from "../../components/dashboard/Steps";
-import Invoice from "../../components/dashboard/Invoice";
+import PriceList from "../../components/dashboard/PriceList.js";
+import ProductList from "../../components/dashboard/ProductList.js";
+import Steps from "../../components/dashboard/Steps.js";
+import Invoice from "../../components/dashboard/Invoice.js";
 
 
 
