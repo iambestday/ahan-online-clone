d8710a6e0a823eedc3081decf968805cdc5e2ffb
iambestday <behrouz.asghari@gmail.com>
2022-09-03 20:50:24 +0430

    ⚡️ Fix

--- src/layouts/dashboard/DashboardLayout.js
+++ src/layouts/dashboard/DashboardLayout.js
@@ -1,11 +1,13 @@
-import Header from "../../components/Header";
-import Dashboard from "../../components/Dashboard";
-import Footer from "../../components/Footer";
+import Header from "../../components/header"
+import Dashboard from "../../components/dashboard"
+import Footer from "../../components/footer"
 
 function DashboardLayout() {
   return (
+
     <div className="container px-2 sm:px-5 sm:py-2">
-      <Header />
+      
+     <Header />
       <Dashboard />
       <Footer />
       </div>
