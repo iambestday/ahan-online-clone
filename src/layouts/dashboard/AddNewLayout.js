78dc7a32d7d8f161d05eca2a11d320d2a1035fcf
iambestday <behrouz.asghari@gmail.com>
2022-09-03 20:11:53 +0430

    🐛

--- src/layouts/dashboard/AddNewLayout.js
+++ src/layouts/dashboard/AddNewLayout.js
@@ -1,19 +1,11 @@
-import PriceList from "../../components/dashboard/PriceList.js";
-import ProductList from "../../components/dashboard/ProductList.js";
-import Steps from "../../components/dashboard/Steps.js";
-import Invoice from "../../components/dashboard/Invoice.js";
+
 
 
 
 function AddNewLayout() {
   return (
     <>
-      <ProductList />
-      <div className="my-4">
-        <PriceList />
-      </div>
-      <Steps />
-    <Invoice />
+
     </>
   )
 }
