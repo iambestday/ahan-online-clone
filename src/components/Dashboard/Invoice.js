3c9a96a48936ddded4e3fd1aa6eccb04fb9f5727
iambestday <behrouz.asghari@gmail.com>
2022-09-03 19:51:35 +0430

    🐛

--- src/components/Dashboard/Invoice.js
+++ src/components/Dashboard/Invoice.js
@@ -1,4 +1,3 @@
-import React from "react";
 import List from "./List"
 import {AiOutlineAudit} from  "react-icons/ai";
 
--- src/layouts/dashboard/AddNewLayout.js
+++ src/layouts/dashboard/AddNewLayout.js
@@ -1,19 +1,19 @@
-import Steps from "../../components/dashboard/Steps";
 import Invoice from "../../components/dashboard/Invoice";
 import PriceList from "../../components/dashboard/PriceList";
 import ProductList from "../../components/dashboard/ProductList";
+import Steps from "../../components/dashboard/Steps";
 
 
 
 function AddNewLayout() {
   return (
     <>
-       <ProductList/>
-       <div className="my-4">
-        <PriceList/>
-        </div>
-        <Steps />
-        <Invoice />
+      <ProductList />
+      <div className="my-4">
+        <PriceList />
+      </div>
+      <Steps />
+    <Invoice/>
     </>
   )
 }
